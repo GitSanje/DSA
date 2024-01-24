dff0903422079dcea98deee566b30e6991e05c2b
unknown <karkisanjay2002@gmail.com>
2024-01-24 23:10:48 +0545

    calculating the max value produced by ai to place in optimal postion and create the simple demo

--- Minmax/minmaxAL.py
+++ Minmax/minmaxAL.py
@@ -1,12 +1,4 @@
 
-ai = 'X'
-player = 'O'
-tic_tac_toe = [['O', 'X',' '],
-               ['X', 'X', 'O'],
-               [' ', 'O', ' '],]
-
-
-
 def checkDiagonal(state):
     L = len(state)
     c = L - 1
@@ -58,42 +50,117 @@ def checkWin(state):
 winners = {'X':1,'O':-1,'d':0}
 
 def Minmax(state,playerTurn):
+
      if checkWin(state):
          return winners.get(checkWin(state))
+     valueai = float("-inf")
+     valuesai = []
+     indexsai = []
 
      if playerTurn == "ai":
-         value = float("-inf")
-         valuesai=[]
-         indexs = []
          for i in range(len(state)):
              for j in range(len(state)):
                    if state[i][j] == " ":
                          state[i][j] = 'X'
-                         print(state,'ai')
-                         v = Minmax(state, 'human')
-                         print(v)
-                         valuesai.append(v)
-                         indexs.append((i,j))
+                         #print(state,'ai')
+                         v_ai= Minmax(state, 'human')
+                         #print(v_ai,'ai')
+                         valuesai.append(v_ai)
+                         indexsai.append((i,j))
                          state[i][j] = ' '
-         # value = max(value,v)
-         return valuesai
+                         valueai = max(valueai,v_ai)
+
+         indx = valuesai.index(max(valuesai))
+
+         return valueai,indexsai[indx]
+
 
      if playerTurn == "human":
-         value = float("inf")
+         valueh = float("inf")
          valuesh=[]
          for i in range(len(state)):
              for j in range(len(state)):
                    if state[i][j] == " ":
                          state[i][j] = 'O'
-                         print(state,'human')
-                         v = Minmax(state, 'ai')
-                         valuesh.append(v)
+                         #print(state,'human')
+                         v_h = Minmax(state, 'ai')
+                         if isinstance(v_h,int):
+                              v_h = v_h
+                         else:
+                             v_h = v_h[0]
+                         # print(v_h)
+                         # print(v_h,'human')
+
+                         valuesh.append(v_h)
                          state[i][j] = ' '
-         # value = min(value,v)
-         return valuesh
+                         valueh = min(valueh,v_h)
+         return valueh
 
 
-print(Minmax(tic_tac_toe,'ai'))
+ai = 'X'
+player = 'O'
+tic_tac_toe = [['O', 'X',' '],
+               ['X', 'X', 'O'],
+               [' ', 'O', ' '],]
+# index =Minmax(tic_tac_toe,'ai')
+# print(index)
+pos = {}
+c=1
+for i in range(3):
+    for j in range(3):
+        pos[c] = (i,j)
+        c+=1
+
+pos2 = {c: (i, j) for c, (i, j) in enumerate(((i, j) for i in range(3) for j in range(3)), start=1)}
+
+
+print("Welcome to minmax game!")
+print("You are human, and your symbol is O")
+print("You are playing against AI, let's see how well you will play")
+print("Enter a number to place the symbol,")
+print("for example, 1 refers to 1st position and so on, only allowed 1 to 9")
+
+while True:
+
+    if(checkWin(tic_tac_toe)):
+         for i in tic_tac_toe:
+            print(i)
+         if checkWin(tic_tac_toe)== 'd':
+             print('The game is draw')
+         elif checkWin(tic_tac_toe)== 'O':
+
+            print('Congratulation! You have won the game')
+         elif checkWin(tic_tac_toe)== 'X':
+             print('ohh! AI have won the game')
+         exit(0)
+    else:
+        for i in tic_tac_toe:
+            print(i)
+        print("No one has won the game")
+        h = int(input("Your turn: "))
+        if h>=1 and h <=9 :
+            x,y = pos.get(h)
+            if tic_tac_toe[x][y] == ' ':
+                tic_tac_toe[x][y] = 'O'
+            else:
+                print("it is not empty")
+                h = int(input("Your turn: "))
+
+
+            res = Minmax(tic_tac_toe,'ai')
+            if isinstance(res,int):
+                continue
+            else:
+                found_key = next((k for k, v in pos.items() if v == res[1]), None)
+                m,n = res[1]
+                tic_tac_toe[m][n] = 'X'
+                print('AI has put X on ', found_key)
+
+
+
+
+
+
 
 # [['O', 'X', ' '],
 #  ['X', 'X', 'O'],
